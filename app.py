4ca81b781f96dd203c71897df013a084e76d81c4
Ananyahp05 <ananyahp580@gmail.com>
2026-02-05 13:13:21 +0530

    my first api

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ client = ChatCompletionsClient(
 
 response = client.complete(
     messages=[
-        SystemMessage("You are a helpful assistant."),
-        UserMessage("What is the capital of France?"),
+        SystemMessage("You are a helpful assistant.always answer in one word"),
+        UserMessage(input("your input: ")),
     ],
     temperature=1.0,
     top_p=1.0,
